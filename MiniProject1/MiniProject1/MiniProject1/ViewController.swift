9283f9b9d817050d603b4c5e7174864d292e20f4
laura-pena <69254524+laura-pena@users.noreply.github.com>
2020-08-05 19:51:07 -0400

    Add files via upload
    Here is my current mini project ( in progress ! )

---
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Laura Pena on 8/5/20.
+//  Copyright © 2020 Laura Pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var clickMe: UIButton!
+    
+    @IBOutlet weak var literatureFact: UILabel!
+    
+    @IBOutlet weak var genreFact: UILabel!
+    
+    @IBOutlet weak var genFact: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
