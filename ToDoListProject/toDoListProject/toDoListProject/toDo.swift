b3a1437c8277b2e8963cd70d76cc09d4e909945a
laura-pena <69254524+laura-pena@users.noreply.github.com>
2020-08-11 09:38:33 -0400

    Completed Iteration 0
    Completed Iteration 0

---
+++ ToDoListProject/toDoListProject/toDoListProject/toDo.swift
@@ -0,0 +1,16 @@
+//
+//  toDo.swift
+//  toDoListProject
+//
+//  Created by Laura Pena on 8/11/20.
+//  Copyright © 2020 Laura Pena. All rights reserved.
+//
+
+import UIKit
+
+class toDoClass {
+    
+    var description = ""
+    var important = false
+    
+}
